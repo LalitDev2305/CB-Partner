aa0035b8795fc0553011f8adab5e1d0a7e58102f
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-15 23:56:32 +0530

    build: migrate core mvi to KMP convention

--- core/mvi/build.gradle.kts
+++ core/mvi/build.gradle.kts
@@ -1,29 +1,11 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidMultiplatformLibrary)
+    id("carbroz.kmp.library")
 }
 
 kotlin {
     android {
         namespace = "com.carbroz.partner.core.mvi"
-        compileSdk = libs.versions.androidCompileSdk.get().toInt()
-        minSdk = libs.versions.androidMinSdk.get().toInt()
-
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
     }
-
-    jvm("desktop") {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
-    }
-
-    iosArm64()
-    iosSimulatorArm64()
 
     sourceSets {
         commonMain.dependencies {
