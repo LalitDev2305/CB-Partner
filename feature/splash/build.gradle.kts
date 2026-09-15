3bd5a82a90b1626eb1c5adf6d49c1c33536e3f79
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-15 23:56:38 +0530

    build: migrate splash to KMP Compose convention

--- feature/splash/build.gradle.kts
+++ feature/splash/build.gradle.kts
@@ -1,31 +1,11 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidMultiplatformLibrary)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    id("carbroz.kmp.compose")
 }
 
 kotlin {
     android {
         namespace = "com.carbroz.partner.feature.splash"
-        compileSdk = libs.versions.androidCompileSdk.get().toInt()
-        minSdk = libs.versions.androidMinSdk.get().toInt()
-
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
     }
-
-    jvm("desktop") {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
-    }
-
-    iosArm64()
-    iosSimulatorArm64()
 
     sourceSets {
         commonMain.dependencies {
