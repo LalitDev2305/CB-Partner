2d219c28e136a53aff05139d4cb111f16fe5ff1a
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-15 23:56:48 +0530

    build: migrate Desktop app to convention

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -1,13 +1,7 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    alias(libs.plugins.kotlinJvm)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
-}
-
-kotlin {
-    jvmToolchain(17)
+    id("carbroz.desktop.application")
 }
 
 dependencies {
