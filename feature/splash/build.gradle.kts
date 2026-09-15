236b901da7bbc529c3d035309c158a132d8f0fdf
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-15 23:32:35 +0530

    build: keep splash as internal KMP feature library

--- feature/splash/build.gradle.kts
+++ feature/splash/build.gradle.kts
@@ -24,15 +24,8 @@ kotlin {
         }
     }
 
-    listOf(
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "FeatureSplash"
-            isStatic = true
-        }
-    }
+    iosArm64()
+    iosSimulatorArm64()
 
     sourceSets {
         commonMain.dependencies {
