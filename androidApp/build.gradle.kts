76fec427a00eeeb9bae832b8cd9fb0ae06831f60
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-15 23:56:43 +0530

    build: migrate Android app to convention

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,27 +1,14 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeCompiler)
+    id("carbroz.android.application")
 }
 
 android {
     namespace = "com.carbroz.partner.android"
-    compileSdk = libs.versions.androidCompileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.carbroz.partner"
-        minSdk = libs.versions.androidMinSdk.get().toInt()
-        targetSdk = libs.versions.androidTargetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    buildFeatures {
-        compose = true
     }
 }
 
